f34783516bf4eceecde7f5a40e7645b51f8433af
Tommaso Passarotto <passarotto13@gmail.com>
2021-12-01 12:12:28 +0100

    inizializzazione 2 macrocomponenti

---
+++ src/components/MyHeader.vue
@@ -0,0 +1,26 @@
+<template>
+  <header>
+    <h1>BoolFlix</h1>
+  </header>
+</template>
+
+<script>
+export default {
+  name: 'MyHeader',
+
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+header {
+  height: 100px;
+  background-color: #000000;
+
+  h1 {
+    color: red;
+    float: left;
+    margin: 25px;
+  }
+}
+</style>
---
+++ src/components/MyMain.vue
@@ -0,0 +1,21 @@
+<template>
+  <main>
+
+  </main>
+</template>
+
+<script>
+export default {
+  name: 'MyMain',
+
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+main {
+    background-color: #434343;
+    // debug
+    height: 100vh;
+}
+</style>
